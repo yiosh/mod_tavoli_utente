ec2ada07536953d7f29322cc3ed54c103e14c77f
yiosh <iinoted@gmail.com>
2018-03-05 19:58:56 +0100

    Remove unnecessary files

--- config/guests_query.php
+++
@@ -1,7 +0,0 @@
-<?php
-  require('db.config.php');
-  // Create Query
-  $queryGuests = $conn->prepare('SELECT * FROM fl_tavoli_commensali WHERE id > 1');// WHERE tavolo_id<>0
-  $queryGuests->execute();
-  // Fetch results
-  $guests = $queryGuests->fetchAll();

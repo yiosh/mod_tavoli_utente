43d1eef3f99061b852970926dd07b2cc60f3a498
yiosh <sigue.nox@gmail.com>
2018-03-07 10:43:35 +0100

    Fix SQL

--- api/tables_submit.php
+++ api/tables_submit.php
@@ -1,23 +1,17 @@
 <?php
+  ini_set('display_errors', 'On');
+  error_reporting(E_ALL | E_STRICT);
   require('../config/db.config.php');
   
   $layout_id = 15;
   $evento_id = 449;
   $tipo_tavolo_id = 2;
   $nome_tavolo = $_POST['nome_tavolo'];
-  // $data_creazione = date_default_timezone_get();
-  // $sql = "SELECT max(numero_tavolo) + 1 FROM fl_tavoli WHERE evento_id = '$evento_id' AND layout_id = '$layout_id'";
-  // $result = mysqli_query($conn, $sql);
-  // $numero_tavolo = mysql_fetch_array($result);
+  $numero_tavolo = $_POST['numero_tavolo'];
+  // $data_creazione = strtotime("now");
   
-  $sql = "INSERT INTO fl_tavoli (layout_id, evento_id, tipo_tavolo_id, nome_tavolo, numero_tavolo)
-  VALUES ('$layout_id', '$evento_id', '$tipo_tavolo_id', '$nome_tavolo', (SELECT max(numero_tavolo) + 1 FROM fl_tavoli WHERE evento_id = $evento_id' AND layout_id = '$layout_id'))";
+  $sql = "INSERT INTO fl_tavoli (numero_tavolo, layout_id, evento_id, nome_tavolo, data_creazione, tipo_tavolo_id)
+  VALUES ('$numero_tavolo', '$layout_id', '$evento_id', '$nome_tavolo', (now()), '$tipo_tavolo_id')";
   $result = mysqli_query($conn, $sql);
-
-  $sql = "SELECT * FROM fl_tavoli";
-  $result = mysqli_query($conn, $sql);
-  $data = mysqli_fetch_assoc($result);
-  echo json_encode($data);
 
   mysqli_close($conn);
-?>

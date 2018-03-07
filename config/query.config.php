f9f8dfc5ba48f2a65da6aaac2a2f8441dfaa3f81
yiosh <sigue.nox@gmail.com>
2018-03-07 10:44:16 +0100

    Change order query results output

--- config/query.config.php
+++ config/query.config.php
@@ -1,10 +1,10 @@
 <?php
   require('db.config.php');
   // Create Query
-  $query_tavoli = "SELECT * FROM fl_tavoli WHERE id > 1";
+  $query_tavoli = "SELECT * FROM fl_tavoli ORDER BY numero_tavolo asc";
   $result_tavoli = mysqli_query($conn, $query_tavoli);
 
-  $query_commensali = "SELECT * FROM fl_tavoli_commensali WHERE id > 1";
+  $query_commensali = "SELECT * FROM fl_tavoli_commensali ORDER BY nome desc";
   $result_commensali = mysqli_query($conn, $query_commensali);
 
 mysqli_close($conn);

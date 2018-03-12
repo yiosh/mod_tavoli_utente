598ec17065ed5f9cb3f4794552f796010e917b7e
yiosh <sigue.nox@gmail.com>
2018-03-12 12:39:11 +0100

    Update script to be less back-end

--- api/guests_delete.php
+++ api/guests_delete.php
@@ -1,15 +1,14 @@
 <?php
   require('../config/db.config.php');
 
-  $id = $_POST['id'];
-
-  // sql to delete a record
-  $sql = "DELETE FROM fl_tavoli_commensali WHERE id='$id'";
-
-  if (mysqli_query($conn, $sql)) {
-      echo "Guest $id successfully deleted";
-  } else {
-      echo "Error deleting Guest $id: " . mysqli_error($conn);
-  }
+	$id = $_POST['id'];
+	
+	// Delete guest
+	$sql = "DELETE FROM fl_tavoli_commensali WHERE id='$id'";
+	if (mysqli_query($conn, $sql)) {
+		echo "cancellato";
+	} else {
+		echo "Errore durante l'eliminazione del ospite $id: " . mysqli_error($conn);
+	}
 
   mysqli_close($conn);
